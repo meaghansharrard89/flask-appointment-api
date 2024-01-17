ce45e22569619412fbb6b372c90e6dd53af00821
Meaghan <meaghan.sharrard89@gmail.com>
2024-01-16 17:57:05 -0800

    attempted lab

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask, make_response, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 import datetime
 from models import db, Doctor, Patient, Appointment
 
@@ -15,13 +16,95 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 
 @app.get("/")
 def index():
     return "doctor/patient"
 
 
+class Doctors(Resource):
+    def get_doctors():
+        doctors = Doctor.query.all()
+        return [d.to_dict(rules=["-appointments"]) for d in doctors], 200
 
+    def post(self):
+        data = request.get_json()
+        doctor = Doctor()
+        try:
+            doctor.name = data.get("name")
+            doctor.specialty = data.get("specialty")
+            db.session.add(doctor)
+            db.session.commit()
+            return make_response(doctor.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(Doctors, "/doctors")
+
+
+class DoctorsById(Resource):
+    def get_doctor_by_id(id):
+        doctor = db.session.get(Doctor, id)
+        if not doctor:
+            return {"error": "not found"}, 404
+        return (
+            doctor.to_dict(
+                rules=["-appointments.patient_id", "-appointments.doctor_id"]
+            ),
+            200,
+        )
+
+
+api.add_resource(DoctorsById, "/doctors/<int:id>")
+
+
+class PatientsById(Resource):
+    def get_patient_by_id(id):
+        patient = db.session.get(Patient, id)
+        if not patient:
+            return {"error": "not found"}, 404
+        patient_dict = patient.to_dict(rules=["-appointments"])
+        doctor_dicts = [d.to_dict(rules=["-appointments"]) for d in patient.doctors]
+        patient_dict["doctors"] = doctor_dicts
+        return patient_dict, 200
+
+    def patch_patient(id):
+        try:
+            data = request.json
+            patient = db.session.get(Patient, id)
+            if not patient:
+                return {"error": "not found"}, 404
+            for key in data:
+                setattr(patient, key, data[key])
+            db.session.add(patient)
+            db.session.commit()
+            return patient.to_dict(rules=["-appointments"])
+        except Exception as e:
+            return {"error": str(e)}, 404
+
+
+api.add_resource(PatientsById, "/patients/<int:id>")
+
+
+class Appointments(Resource):
+    def post(self):
+        data = request.get_json()
+        appointment = Appointment()
+        try:
+            appointment.day = data.get("day")
+            appointment.doctor_id = data.get("doctor_id")
+            appointment.patient_id = data.get("patient_id")
+            db.session.add(appointment)
+            db.session.commit()
+            return make_response(appointment.to_dict(), 201)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(Appointments, "/appointments")
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -15,17 +15,78 @@ db = SQLAlchemy(metadata=metadata)
 
 class Patient(db.Model, SerializerMixin):
     __tablename__ = "patient_table"
-    id = db.Column(db.Integer, primary_key=True)
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    # Relationships
+
+    appointments = db.relationship(
+        "Appointment", back_populates="patient", cascade="all, delete-orphan"
+    )
+    doctors = association_proxy("appointments", "doctor")
+
+    # Serialization rules
+
+    serialize_rules = ("-appointments.patient",)
 
 
 class Appointment(db.Model, SerializerMixin):
     __tablename__ = "appointment_table"
+
     id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String, nullable=False)
 
+    # Foreign keys
 
+    doctor_id = db.Column(db.Integer, db.ForeignKey("doctor_table.id"), nullable=False)
+    patient_id = db.Column(
+        db.Integer, db.ForeignKey("patient_table.id"), nullable=False
+    )
+
+    # Relationships
+
+    patient = db.relationship("Patient", back_populates="appointments")
+    doctor = db.relationship("Doctor", back_populates="appointments")
+
+    # Serialization rules
+
+    serialize_rules = (
+        "-patient.appointments",
+        "-doctor.appointments",
+    )
+
+    # Validation
+
+    @validates("day")
+    def validate_day(self, key, value):
+        if value not in self.day_choices:
+            raise ValueError("Invalid day. Must be between Monday and Friday.")
+        return value
 
 
 class Doctor(db.Model, SerializerMixin):
     __tablename__ = "doctor_table"
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    specialty = db.Column(db.String, nullable=False)
+
+    # Relationships
+
+    appointments = db.relationship(
+        "Appointment", back_populates="doctor", cascade="all, delete-orphan"
+    )
+    patients = association_proxy("appointments", "patient")
+
+    # Serialization rules
+
+    serialize_rules = ("-appointments.doctor",)
+
+    # Validation
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if not value.startswith("Dr."):
+            raise ValueError("Doctor's name must start with 'Dr.'")
+        return value
